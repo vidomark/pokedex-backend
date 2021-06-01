e4cb3d1c9dd084a1f8426a62f09016ea9052279e
Vidó Márk <vidomark42@gmail.com>
2021-06-01 11:43:15 +0200

    Form - form of pokemon implemented

--- src/main/java/com/codecool/pokedex/controller/PokemonController.java
+++ src/main/java/com/codecool/pokedex/controller/PokemonController.java
@@ -2,6 +2,7 @@ package com.codecool.pokedex.controller;
 
 import com.codecool.pokedex.dao.util.PokemonUtil;
 import com.codecool.pokedex.model.Ability;
+import com.codecool.pokedex.model.Form;
 import com.codecool.pokedex.model.Pokemon;
 import com.codecool.pokedex.service.PokemonService;
 import org.json.JSONArray;
@@ -32,7 +33,7 @@ public class PokemonController {
     public void getPokemons() throws IOException {
         String endpoint = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=18";
         JSONObject data = PokemonUtil.fetchData(endpoint);
-        JSONArray pokemons = (JSONArray) data.get("results");
+        JSONArray pokemons = data.getJSONArray("results");
         pokemons.forEach(pokemonData -> {
             JSONObject pokemonJson = (JSONObject) pokemonData;
             String pokemonUrl = pokemonJson.getString("url");
@@ -48,21 +49,27 @@ public class PokemonController {
 
     private void createPokemon(JSONObject json) {
         String pokemonName = json.getString("name");
-        JSONArray abilitiesJson = (JSONArray) json.get("abilities");
-        List<Ability> abilities = createAbilities(abilitiesJson);
-
+        List<Ability> abilities = createAbilities(json);
+        Form form = createForm(json);
     }
 
-    private List<Ability> createAbilities(JSONArray abilitiesJson) {
+    private List<Ability> createAbilities(JSONObject json) {
+        JSONArray abilitiesJson = json.getJSONArray("abilities");
         return abilitiesJson.toList().stream().map(data -> {
-            HashMap abilityMap = (HashMap) data;
-            HashMap<String, String> abilityData = (HashMap<String, String>) abilityMap.get("ability");
+            HashMap dataMap = (HashMap) data;
+            HashMap<String, String> abilityMap = (HashMap<String, String>) dataMap.get("ability");
 
-            String name = abilityData.get("name");
-            String url = abilityData.get("url");
-            boolean isHidden = (boolean) abilityMap.get("is_hidden");
-            int slot = (int) abilityMap.get("slot");
+            String name = abilityMap.get("name");
+            String url = abilityMap.get("url");
+            boolean isHidden = (boolean) dataMap.get("is_hidden");
+            int slot = (int) dataMap.get("slot");
             return new Ability(name, isHidden, slot, url);
         }).collect(Collectors.toList());
+    }
+
+    private Form createForm(JSONObject json) {
+        JSONObject formJson = json.getJSONArray("forms").getJSONObject(0);
+        String name = formJson.getString("name");
+        return Enum.valueOf(Form.class, name.toUpperCase());
     }
 }
--- src/main/java/com/codecool/pokedex/model/Pokemon.java
+++ src/main/java/com/codecool/pokedex/model/Pokemon.java
@@ -1,7 +1,10 @@
 package com.codecool.pokedex.model;
 
+import java.util.List;
+
 public class Pokemon {
 
     private String name;
-    private Ability ability;
+    private List<Ability> abilities;
+    private Form form;
 }
