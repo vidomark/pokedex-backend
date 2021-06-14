4c4a2cde627a99700c19e46da77ed48913e6bbd6
Vidó Márk <vidomark42@gmail.com>
2021-06-14 18:23:15 +0200

    Pokemon fetch - fetching pokemons outsourced into pokemon container

--- src/main/java/com/codecool/pokedex/dao/pokemon/util/PokemonUtil.java
+++ src/main/java/com/codecool/pokedex/dao/pokemon/util/PokemonUtil.java
@@ -13,24 +13,23 @@ import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
 
 public class PokemonUtil {
-    private static URL url;
 
     public static JSONObject fetchData(String endpoint) throws IOException {
-        url = new URL(endpoint);
-        URLConnection connection = url.openConnection();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
-        String lines = reader.lines().collect(Collectors.joining("\n"));
-        reader.close();
-        JSONObject json = new JSONObject(lines);
-        return json;
+        String jsonString = getJsonString(endpoint);
+        return new JSONObject(jsonString);
     }
 
     public static Pokemon fetchPokemon(String endpoint) throws IOException {
-        url = new URL(endpoint);
+        String lines = getJsonString(endpoint);
+        return new Gson().fromJson(lines, Pokemon.class);
+    }
+
+    private static String getJsonString(String endpoint) throws IOException {
+        URL url = new URL(endpoint);
         URLConnection connection = url.openConnection();
         BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
-        String lines = reader.lines().collect(Collectors.joining("\n"));
+        String jsonString = reader.lines().collect(Collectors.joining("\n"));
         reader.close();
-        return new Gson().fromJson(lines, Pokemon.class);
+        return jsonString;
     }
 }
